faeb37ceb8833b7116f332eabe64bc9a1a4e9ba6
Bram Dingelstad <git@dingelstad.works>
2023-07-05 12:54:23 +0200

    feat: implemented setting state

--- Dialogue.gd
+++ Dialogue.gd
@@ -2,6 +2,9 @@ extends Control
 
 onready var template = $VBoxContainer/Template
 
+const DAGGER_IS_HIGH_QUALITY = 1
+const DAGGER_IS_LOW_QUALITY = 2
+
 func _ready():
 	$VBoxContainer.remove_child(template)
 
@@ -9,24 +12,26 @@ func _ready():
 	$Interpreter.connect('choices', self, '_on_choices')
 	$Interpreter.connect('stopped', self, '_on_stopped')
 
-	# NOTE: You can also just add the Database in your scene instead of as an AutoLoad, and refer to it with $Database
-	print(Database.get_models_of_type("Customers")[0])
-	print(Database.get_models_of_type("Dialogue")[0])
+	# NOTE: You can also just add the Database in as an AutoLoad instead of your scene and refer to it with Database
+	print($Database.get_models_of_type("Customers")[0])
+	print($Database.get_models_of_type("Dialogue")[0])
+
+	var groundskeeper_done = "0x0100000100000481"
+
 
 	# Day 1
-	$Interpreter.start(Database.get_available_dialogues()[0].id)
+	$Interpreter.set_state('quality.groundskeeper_dagger', DAGGER_IS_LOW_QUALITY)
+	$Interpreter.start(groundskeeper_done)
 
 
 func _on_line(data):
-	print(data)
 	# NOTE: Wait one frame to prevent re-entrant problems with GDNative
 	yield(get_tree(), 'idle_frame')
-	var speaker = Database.get_model(data.speaker)
+	var speaker = $Database.get_model(data.speaker)
 	$RichTextLabel.bbcode_text = data.line
 
 
 func _on_choices(data):
-	print(data)
 	var index = 0
 	for choice in data:
 		var button = template.duplicate()
